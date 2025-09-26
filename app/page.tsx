b31ff18bf50668e3d10ec8ec8a6607eef19c86c6
Shoo <snakexxs80@gmail.com>
2025-09-26 17:47:10 -0600

    Se agrego una dependencia para las analiticas de la pagina

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import Hero from "@/components/hero"
 import Organigrama from "@/components/organigrama"
 import Enlaces from "@/components/enlaces"
 import Footer from "@/components/footer"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className="overflow-x-hidden">
+      <SpeedInsights />
       <Navbar />
       <Hero />
       <Organigrama />
